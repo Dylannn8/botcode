e6febe98dc74d576c036a1416fbc41e1557ae8f8
Zszs2222 <74486569+Dylannn8@users.noreply.github.com>
2025-03-13 12:17:45 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+npm install express
+npm install ngrok
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz
+tar -xvzf ngrok-v3-stable-linux-arm64.tgz
+mv ngrok /data/data/com.termux/files/usr/bin
+chmod +x /data/data/com.termux/files/usr/bin/ngrok
+mkdir -p ~/.config/ngrok
+ngrok --version
+clear
+echo "VERIFICA" 
+echo "SI NGROK" 
+echo "SE INSTALO BIEN" 
+sleep 5 
+echo "BOT BY DYLAN" 
